539b1850cd1cff0cffe9865951e1e305345f1c90
T2LIPthedeveloper <77957349+T2LIPthedeveloper@users.noreply.github.com>
2023-05-07 11:47:43 +0530

    Initial Commit

---
+++ BaseChatbot/BaseChatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseChatbotApp.swift
+//  BaseChatbot
+//
+//  Created by Atul Parida on 07/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BaseChatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
