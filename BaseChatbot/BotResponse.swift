183bbd98f882a0be2a173a3d2f94f79123c1b032
T2LIPthedeveloper <77957349+T2LIPthedeveloper@users.noreply.github.com>
2023-06-06 18:36:21 +0800

    App - uglified

--- BaseChatbot/BotResponse.swift
+++ BaseChatbot/BotResponse.swift
@@ -7,24 +7,28 @@
 
 import Foundation
 
-var responseDict: [String: String] = [
+var responseDict: [String: [String]] = [
     // This is the list of keywords and responses we want our chatbot to respond to
     // When you type something, if the bot finds a keyword from within your message, it'll respond with the first keyword it finds
     // Format: "keyword" : "Response for that keyword", separate pairs with a comma
-    "hello": "Hey there, I'm a chatbot! How can I help you today?",
-    "how are you": "I'm doing good, what about you?",
-    "good": "That's good!",
-    "bad": "Uh oh!",
-    "bye": "Alright, talk to you later!"
+    "hello": ["Hey there, I'm a chatbot! How can I help you today?", "Hi there!", "What's up?", "How's it going?"],
+    "how are you": ["I'm doing good, what about you?", "All good, and you?", "Doing great! How are you doing?"],
+    "good": ["That's good!", "Nice!", "That's cool!"],
+    "bad": ["Uh oh!", "Oh no!", "Oof...", "Oops!"],
+    "bye": ["Alright, talk to you later!", "Okay, see you!", "Bye bye!", "See you later, alligator!"]
+    
 ]
 
 func getBotResponse(message: String) -> String {
     let tempMessage = message.lowercased() // converts our message to lowercase so it's easier to search
-    var response: String = String() //empty response
+    var response: String = String() // empty response
     for key in responseDict.keys {
         if tempMessage.contains(key) {
-            response = responseDict[key] ?? ""
-            //If there's a keyword, we find the first matching response and assign it, else assign nothing
+            let randomInt = Int.random(in: 0..<(responseDict[key]?.count ?? 1))
+            response = responseDict[key]?[randomInt] ?? ""
+            // If there's a keyword, we find the array corresponding to the first matching one.
+            // If the array within contains more than one element, we choose a random index otherwise we just choose the first thing available
+            
             break // Stops as soon as we find the first matching keyword
         }
     }
--- BaseChatbot/ContentView.swift
+++ BaseChatbot/ContentView.swift
@@ -19,61 +19,23 @@ struct ContentView: View {
             //Our app header and probably an icon.
             HStack {
                 Text("Chatbot")
-                    .font(.largeTitle)
-                    .bold()
-                
-                Image(systemName: "bubble.left.fill")
-                    .font(.system(size: 25))
-                    .foregroundColor(Color.blue)
             }
             
             //This is where our messages go!
             ScrollView {
                 // We're going to go through the list of all messages and show them on the screen.
                 ForEach(messages, id: \.self) { message in
-                    if message.contains("[USER]") {
-                        let newMessage = message .replacingOccurrences(of: "[USER]", with: "") //removes the [USER] tag from user messages
-                        
-                        //UI for user message bubble
-                        HStack {
-                            Spacer()
-                            //The spacer here takes up all the space before the message, pushing it to the right.
-                            Text(newMessage)
-                                .padding()
-                                .foregroundColor(.white)
-                                .background(.blue.opacity(0.8))
-                                .cornerRadius(10)
-                                .padding(.horizontal, 16)
-                                .padding(.bottom, 10)
-                        }
-                        
-                    }
-                    else {
-                        //UI for bot message bubble
-                        HStack {
-                            Text(message)
-                                .padding()
-                                .foregroundColor(.black)
-                                .background(.gray.opacity(0.15))
-                                .cornerRadius(10)
-                                .padding(.horizontal, 16)
-                                .padding(.bottom, 10)
-                            Spacer()
-                            //The spacer here takes up all the space after the message, pushing it to the left.
-                        }
-                    }
+                    Text(message)
+                    
                 }.rotationEffect(.degrees(180))
             }.rotationEffect(.degrees(180))
-            .background(Color.gray.opacity(0.05))
             // rotates the text boxes inside, brings them to the bottom of the screen
-            
             
             // Our text message box and send button
             HStack {
                 TextField("Type something!", text: $messageText)
                     .padding()
-                    .background(Color.gray.opacity(0.1))
-                    .cornerRadius(10)
+                    .background(.gray.opacity(0.2))
                     .onSubmit {
                         // This is what happens when the text gets submitted!
                         sendMessage(message: messageText)
@@ -82,9 +44,7 @@ struct ContentView: View {
                     // sends a message to the chatbot
                     sendMessage(message: messageText)
                 } label: {
-                    Image(systemName: "paperplane.fill")
-                        .font(.system(size: 25))
-                        .padding(.horizontal, 10)
+                    Text("Send")
                     // image for your send button
                 }
             }
@@ -95,19 +55,9 @@ struct ContentView: View {
     func sendMessage(message: String) {
         if !message.isEmpty {
             //if the message is empty, we don't respond.
-            
-            withAnimation {
-                messages.append("[USER]" + message) //adds a message to the list of messages
-                self.messageText = "" //resets the message box
-            }
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                // 1 second delay until the message gets sent, you don't need to worry about this for now.
-                withAnimation {
-                    messages.append(getBotResponse(message: message))
-                    //adds a bot message to the list of messages
-                }
-            }
+            messages.append("[USER]" + message)
+            self.messageText = ""
+            messages.append(getBotResponse(message: message))
         }
     }
     
@@ -118,3 +68,51 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+/*
+ if message.contains("[USER]") {
+     let newMessage = message .replacingOccurrences(of: "[USER]", with: "") //removes the [USER] tag from user messages
+     
+     //UI for user message bubble
+     HStack {
+         Spacer()
+         //The spacer here takes up all the space before the message, pushing it to the right.
+         Text(newMessage)
+             .padding()
+             .foregroundColor(.white)
+             .background(.blue.opacity(0.8))
+             .cornerRadius(10)
+             .padding(.horizontal, 16)
+             .padding(.bottom, 10)
+     }
+     
+ }
+ else {
+     //UI for bot message bubble
+     HStack {
+         Text(message)
+             .padding()
+             .foregroundColor(.black)
+             .background(.gray.opacity(0.15))
+             .cornerRadius(10)
+             .padding(.horizontal, 16)
+             .padding(.bottom, 10)
+         Spacer()
+         //The spacer here takes up all the space after the message, pushing it to the left.
+     }
+ }
+ 
+ withAnimation {
+     messages.append("[USER]" + message) //adds a message to the list of messages
+     self.messageText = "" //resets the message box
+ }
+ 
+ DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+     // 1 second delay until the message gets sent, you don't need to worry about this for now.
+     withAnimation {
+         messages.append(getBotResponse(message: message))
+         //adds a bot message to the list of messages
+     }
+ }
+ 
+ */
